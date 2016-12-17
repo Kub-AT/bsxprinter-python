95b71e235ab63b0e3a51a86d0b53d61d62a71e95
Jakub Stawowy <kubas1989@gmail.com>
2016-12-17 16:38:20 +0100

    fix tests

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ from bsxprinter.formatters import XMLFormatter
 class ReceiptTest(unittest.TestCase):
 
     def test_it_should_add_items(self):
-        receipt = Receipt()
+        receipt = Receipt(1)
         self.assertEqual(receipt.items, [])
         receipt.add_item('Item 1', 19, 1, 23)
         self.assertEqual(len(receipt.items), 1)
@@ -18,7 +18,7 @@ class ReceiptTest(unittest.TestCase):
         self.assertEqual(len(receipt.items), 3)
 
     def test_is_should_iter_over_items(self):
-        receipt = Receipt()
+        receipt = Receipt(1)
         receipt.add_item('Item 2', 99, 1, 23).add_item('Item 3', 149.95, 2, 23)
         for r in receipt:
             pass
